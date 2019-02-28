e4808ef1fc51714d0e2d3d4d2423323f1e8abbc2
Hector-Josue7 <hectorardonm@gmail.com>
2019-02-28 15:02:58 -0600

    validaciones primera version

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ var app = express();
 
 //definir una carpeta como publica para que los usuarios puedan acceder a su contenido
 app.use(express.static("public"));
+
+
+
+var credenciales = {
+  host:"localhost",
+  user:"root",
+  password:"",
+  port:"3306",
+  database: "bd_proyecto"
+};
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
  
 //Levantar el servidor en el puerto 3333
 app.listen(3333, function(){
     console.log("Servidor levantado en el puerto 3333");
 });
+
+var mysql = require('mysql');
+
+
+var conexion = mysql.createConnection(credenciales);
