9ebb2e4a1a9ca7a9ff45c7f38c881e48de172393
Hector-Josue7 <hectorardonm@gmail.com>
2019-03-11 11:35:29 -0600

    Correccion de errores de indexedDB

--- public/js/controlador.js
+++ public/js/controlador.js
@@ -1,37 +1,53 @@
-var db;
 
-(function(){
-    if (!('indexedDB' in window)){
-        console.err("El navegador no soporta indexedDB");
-        return;
-    }
+//PROBANDO UTILIZAR INDEXED_DB PARA EL FORMULARIO DE REGISTRO CON LOS CAMPOS NOMBRE, APELLIDO Y CORREO
+var bd;
+function iniciar(){
 
-    //open crea la base de datos si no existe, en caso contrario la abre para utilizarla
-    var solicitud = window.indexedDB.open("PruebaFormularios", 1);//Parametros: nombre, version. La version debe ser entero
+    botonEnviar =document.getElementById("botonEnviar");
     
-    solicitud.onsuccess = function(evento){
-        db = solicitud.result;
-        console.log("Se abrio o se creo la BD");
-        actualizarTabla();
-    }
+ botonEnviar.addEventListener("click", agregarUsuario, false);
+ var solicitud = indexedDB.open("baseUsuariosRegistrados");
+ solicitud.onsuccess= function(e){
+     bd=e.target.result;
+ }
+ solicitud.onupgradeneeded= function (e) {
+     bd=e.target.result;
+     bd.createObjectStore("usuarios", {keyPath: "clave"});
+     
+ }
 
-    solicitud.onerror = function(evento){
-        console.error(evento);
-    }
+} 
 
-    //Evento que se ejecuta cuando se crea  o actualiza la base datos
-    solicitud.onupgradeneeded = function(evento){
-        console.log('Se creo o actualizo la BD');
-        
-        db = evento.target.result;//Obtener referencia de la BD creada
-        //Crear contenedores de objetos (ObjectStores)
-        var objectStoreUsuarios = db.createObjectStore("usuarios", {keyPath: "codigo", autoIncrement: true});
-        objectStoreUsuarios.transaction.oncomplete = function(evento){
-            console.log("El object store de usuarios se creo con exito");
-        }
-        objectStoreUsuarios.transaction.onerror = function(evento){
-            console.log("Error al crear el object store de usuarios");
-        }
-    }
 
-})();
+function agregarUsuario(){
+
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    
+    
+    var transaccion = bd.transaction(["usuarios"], "readwrite");
+    var almacen = transaccion.objectStore("usuarios");
+    var agregar = almacen.add({ nombre: nombre,
+                                 apellido: apellido,  
+                                 correo: correo});
+      agregar.addEventListener("success", mostrar, false);                          
+     document.getElementById("nombre").value=""
+     document.getElementById("apellido").value=""
+     document.getElementById("correo").value=""                           
+    
+    
+       }
+       function mostrar(){
+        zonadatos.innerHTML="";
+        var transaccion = bd.transaction(["usuarios"], "readonly");
+        var almacen = transaccion.objectStore("usuarios");
+        var cursor = almacen.openCursor();
+        cursor.addEventListener("success", mostrarDatos, false);
+    }
+    function mostrarDatos(e){
+        var cursor=e.target.result;
+      
+   }
+   window.addEventListener("load",iniciar, false);
+// FIN DEL CODIGO INDEXED_DB PARA EL FOMULARIO REGISTRO
